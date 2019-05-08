040fd6d938ad7363d89c91c32d96dc7f355280d3
IsraelChicago <rabbi.yi.hass@gmail.com>
2019-05-08 13:20:27 -0500

    Adds Products to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.create(name: "Apple", price: 1.20, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2NZsWqDrwzC6PsDNXomi_Ibp9OraAY2341rxbMY8qoNGG1Ugo", describtion: "A red shiny delicious fruit")
+
+
+product = Product.create(name: "Pear", price: 1, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsQJSbcXdRRqi3m_6Cdn-mee4q83UKKYui2JjAyCk2TBAsoMHA", describtion: "A sweet fruit")
+
+
